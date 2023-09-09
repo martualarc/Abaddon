96dd98f74d2ee26fc8275f879bd5c0b493d95ae7
Ramiro Marquez <114887564+ramamarquez2@users.noreply.github.com>
2023-09-09 03:40:49 -0300

    Bugfix&cleaning/logica-flashlight

--- Assets/Codes/Scripts/Flashlight.cs
+++ Assets/Codes/Scripts/Flashlight.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class Flashlight: MonoBehaviour {
     //inicializar acceso a scripts de demonios
-    //inicializar fisicas del raycast y las variables necesarias
-    //junto a la creacion de un layer de demonios
+    //inicializar fisicas del raycast y las variables necesarias:
+    [SerializeField] private float interactDistance = 4f;
+    [SerializeField] private int layer = 5;
+    private int interactLayers;
+    //creacion de un layer de demonios
     public bool isFlashing;
     public bool flashOn;
     public float timeFlashing;
     public float timeNotFlashing;
     
     void Start () {
+        demonLayers = (1 << layer);
+        demonLayers = ~interactLayers;
 
         isFlashing = false;
         timeFlashing = 0;
@@ -21,15 +26,17 @@ public class Flashlight: MonoBehaviour {
     
     void Update () {
         
-        //rastrear posicion del jugador y actualizar la posicion de la linterna en funcion a ello
-        
-        if (flashOn == true)
+        if(flashOn)
         {
             flash();
-            //la linterna esta encendida
+            //renderLuzLinterna = True : la linterna esta encendida
+        }
+        else
+        {
+            isFlashing = false;
         }
         
-        if (isFlashing)
+        if(isFlashing)
         {
             timeNotFlashing = 0;
             timeFlashing += time.deltaTime;
@@ -39,36 +46,21 @@ public class Flashlight: MonoBehaviour {
             timeFlashing = 0;
             timeNotFlashing += time.deltaTime;
         }
-
-
-    /*
-    funciones que pueden usarse en flash():
-    Componente <Demonio>:
-        Demonio.infligirDaño(int)
-    */
-    /*
-    ideas:
-    1) collider solo en la parte vulnerable del demonio
-    2) if( distancia-de-la-colision < demonio5.longWarning && 1 )
-        then demonio5.segundosAntesDeMorir --; 
-    2 alternativo) medir angulo del vector flash con respecto al piso
-    3) personalizar para cada demonio
-    4) acceso hacia y desde a scripts Demonio
-    */
-    //void flash(){
-        /*logica de apuntar al demonio
+    }
+    
+    void flash(){ //logica de apuntar al demonio
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, interactDistance, demonLayers)){
+        if (Physics.Raycast(transform.position, transform.forward, out hit, interactDistance, demonLayers))
+        {
             Demonio scriptDem = hit.collider.GetComponent<Demonio>(); //guarda cualquier objeto con clase heredada de demonio
             if (scriptDem != null)
             {
                 isFlashing = true;
             }
-            else
-            {
-                isFlashing = false;
-            }
-        */
-        
-    //}
-//}
+        }
+        else
+        {
+            isFlashing = false;
+        }
+    }
+}
