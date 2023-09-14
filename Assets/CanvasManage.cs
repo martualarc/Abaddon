bfbb3b40d88481f180400893dac721806a13b2c5
martualarc <martina.alarcon029@gmail.com>
2023-09-14 02:51:58 -0300

    Canvas con UI se transporta entre escenas

---
+++ Assets/CanvasManage.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasManage : MonoBehaviour
+{
+    public static CanvasManage instance;
+    void Start()
+    {
+        if(instance != null){
+            Destroy(gameObject);
+        }
+        else {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
