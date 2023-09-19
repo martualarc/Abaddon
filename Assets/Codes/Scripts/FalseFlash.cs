aeb21ce15d04272a2d19466a794d56d6587dfed9
Ramiro Marquez <114887564+ramamarquez2@users.noreply.github.com>
2023-09-19 09:26:55 -0300

    Update UI flash

--- Assets/Codes/Scripts/FalseFlash.cs
+++ Assets/Codes/Scripts/FalseFlash.cs
@@ -1,37 +1,30 @@
-
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class FalseFlash: MonoBehaviour {
     public GameObject flashObj;
+    public Image flashUI;
     Flashlight scriptFlash; //guarda el objeto para su posterior uso (TryFlash)
 
 }
-/*
-flashObj tiene que ser el nombre utilizado en el editor de Unity? No.
-Nombre en el Inspector: En el Inspector de Unity, cuando agregas el 
-script a un GameObject, verás la variable flashObj en el Inspector. 
-Puedes arrastrar y soltar el GameObject que tiene el script
- "Flashlight" adjunto en esa variable.
-*/
-/*
-    Renderer falseRender; //render linterna de juguete
+    //Renderer falseRender; //render linterna de juguete
     Renderer flashRender;
 
     void Start(){
         scriptFlash = flashObj.GetComponent<Flashlight>();
 
-        falseRender = gameObject.GetComponent<Renderer>();
+        //falseRender = gameObject.GetComponent<Renderer>();
         flashRender = flashObj.GetComponent<Renderer>();
         flashRender.enabled = false;
+        flashUI.gameObject.SetActive(false);
     }
 
     Flashlight interact(){
-        falseRender.enabled = false;
-        //agregar codigo sobre UI de icono de la linterna
+        //falseRender.enabled = false;
+        flashUI.gameObject.SetActive(true);
         flashRender.enabled = true; //encender visualizacion del modelado de la linterna in-game
         return scriptFlash;
     }
 
 }
-*/
