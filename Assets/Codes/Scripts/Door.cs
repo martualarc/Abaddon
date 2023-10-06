e566278d8f14f371a99820f56d5c2cf8c93ad057
Bautista Renaudo <84209158+BautiRenaudo9-12@users.noreply.github.com>
2023-10-06 16:43:45 -0300

    implement door and key scripts

--- Assets/Codes/Scripts/Door.cs
+++ Assets/Codes/Scripts/Door.cs
@@ -1,28 +1,42 @@
 using UnityEngine;
 using System.Collections;
 
-public class Door : MonoBehaviour {
+public class Door : MonoBehaviour
+{
 
     public int num;
     public bool isOpenable = true;
 
-    void Start() {
+    void Start()
+    {
         GetComponent<sceneExit>().enabled = false;
     }
-    
-    void Update() {
-     
+
+    void Update()
+    {
+
     }
 
-    public bool interact(bool keyIsKey, int keyNum) {
-        if(isOpenable && keyIsKey && keyNum == num ) {
+    public bool interact(bool keyIsKey, int keyNum)
+    {
+        if (isOpenable && keyIsKey && keyNum == num)
+        {
             isOpenable = false;
+            OpenDoor(); // Llamamos a la función para abrir la puerta
             return true;
         }
-        else {
+        else
+        {
+            Debug.Log("La puerta está cerrada o necesitas una llave.");
             return false;
             //agregar logica para que te avise que te falta la llave o que te diga que ya no se puede entrar
         }
+    }
+    void openDoor()
+    {
+        // lógica para la animación de abrir la puerta
+        // animación simple: abrir la puerta girándola sobre su eje Y durante 1 segundo.
+        transform.Rotate(Vector3.up, 90f, Space.World);
     }
     //void openDoor funcion que realiza la animacion de abrirla
 }
--- Assets/Codes/Scripts/Key.cs
+++ Assets/Codes/Scripts/Key.cs
@@ -1,29 +1,64 @@
 using UnityEngine;
 using System.Collections;
 
-public class Key : MonoBehaviour {
+public class Key : MonoBehaviour
+{
 
     public int num;
     public bool isKey;
 
-    void Start () {
-    //apuntar hacia el objeto del icono de la key en la interfaz
+    private GameObject keyIcon; // referencia al objeto del icono de la llave en la interfaz
+
+    void Start()
+    {
+        // buscar el objeto del icono de la llave en la interfaz por su nombre
+        keyIcon = GameObject.Find("KeyIcon"); // asegurarse de que el nombre coincida con el objeto en tu escena
+
+        if (keyIcon == null)
+        {
+            Debug.LogError("No se pudo encontrar el objeto del icono de la llave en la interfaz.");
+        }
+        else
+        {
+            // el objeto del icono de la llave fue encontrado
+            Debug.Log("¡Objeto del icono de la llave encontrado!");
+        }
+
+        //apuntar hacia el objeto del icono de la key en la interfaz
     }
-    
-    void Update () {
-     
+
+    void Update()
+    {
+
     }
-    public void changeNum(int nextDoor) {
+
+
+    public void collectKey()
+    {
+        // reproducir un sonido de recogida
+        AudioManager.Instance.PlayCollectKeySound();
+        // mostrar un mensaje en la consola
+        Debug.Log("¡Has recogido la llave #" + num);
+
+        // función para recoger la llave
+    }
+    public void changeNum(int nextDoor)
+    {
         num = nextDoor + 1;
         isKey = false;
+
         //funcion que decolorea la key en la interfaz
     }
-    public void getKey() { //funcion llamada por realizar puzzles o vencer demonios
+    public void getKey()
+    {
         isKey = true;
+
+        //funcion llamada por realizar puzzles o vencer demonios
         //funcion que colorea la key en la interfaz
     }
-    
-    public void keyRespawn(){
+
+    public void keyRespawn()
+    {
         num -= 1;
         getKey();
     }
