910d37656aa0655233344415b4a0b3320f424e08
Ramiro Marquez <114887564+ramamarquez2@users.noreply.github.com>
2023-10-13 11:04:19 -0300

    Decolorear key UI cuando se usa & update comments

--- Assets/Codes/Scripts/Key.cs
+++ Assets/Codes/Scripts/Key.cs
@@ -13,46 +13,26 @@ public class Key : MonoBehaviour
 
     void Start()
     {
-        // buscar el objeto del icono de la llave en la interfaz por su nombre
-        keyIcon = GameObject.FindWithTag("UIKey").GetComponent<Image>(); // asegurarse de que el nombre coincida con el objeto en tu escena
-
-        if (keyIcon == null)
-        {
-            Debug.LogError("No se pudo encontrar el objeto del icono de la llave en la interfaz.");
-        }
-        else
-        {
-            // el objeto del icono de la llave fue encontrado
-        }
-
+        keyIcon = GameObject.FindWithTag("UIKey").GetComponent<Image>();
         //apuntar hacia el objeto del icono de la key en la interfaz
     }
-
-    void Update()
-    {
-
-    }
-
 
     public void collectKey()
     {
         // reproducir un sonido de recogida
         //StartCoroutine(Audio.Sonido(keySound, 0f));
-        // mostrar un mensaje en la consola
         Debug.Log("¡Has recogido la llave #" + num);
-
-        // función para recoger la llave
+        // función en PlayerInteract para recoger la llave
     }
     public void changeNum(int nextDoor)
     {
         num = nextDoor + 1;
         isKey = false;
-
-        //funcion que decolorea la key en la interfaz
+        keyIcon.GetComponent<CanvasRenderer>().SetAlpha(0f);
     }
     public void getKey()
     {
-         Debug.Log("¡Has recogido la llave #" + num);
+        Debug.Log("¡Has recogido la llave #" + num);
         isKey = true;
         keyIcon.GetComponent<CanvasRenderer>().SetAlpha(1f);
 
@@ -64,5 +44,6 @@ public class Key : MonoBehaviour
     {
         num -= 1;
         getKey();
+        //funcion llamada al reaparecer, falta la logica de la door
     }
 }
