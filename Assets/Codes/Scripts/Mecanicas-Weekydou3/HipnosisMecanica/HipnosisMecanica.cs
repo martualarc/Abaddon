59adf20f923277f246a85f88a47de958be683e29
BautiRenaudo9-12 <renaudobautista@gmail.com>
2023-10-18 15:22:50 -0300

    mecanica hipnosis arreglo

--- Assets/Codes/Scripts/Mecanicas-Weekydou3/HipnosisMecanica/HipnosisMecanica.cs
+++ Assets/Codes/Scripts/Mecanicas-Weekydou3/HipnosisMecanica/HipnosisMecanica.cs
@@ -2,20 +2,19 @@ using UnityEngine;
 
 public class HipnosisMecanica : MonoBehaviour
 {
-    public Transform pantallaDeHipnosis; // Referencia al objeto que representa la pantalla de hipnosis
     public float tiempoRequerido = 15f; // Tiempo requerido en segundos para completar la hipnosis
     private float tiempoActual; // Tiempo transcurrido
-    private bool estaHipnotizado; // Indica si el jugador está hipnotizado
+
+    public Camera viewCamera;
 
     private void Start()
     {
         tiempoActual = 0f;
-        estaHipnotizado = false;
     }
 
     private void Update()
     {
-        if (estaHipnotizado)
+        if (EstaMirandoObjeto())
         {
             tiempoActual += Time.deltaTime;
 
@@ -30,25 +29,32 @@ public class HipnosisMecanica : MonoBehaviour
         }
     }
 
+    private bool EstaMirandoObjeto()
+    {
+        Ray gazeRay = new Ray(viewCamera.transform.position, viewCamera.transform.rotation * Vector3.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(gazeRay, out hit, Mathf.Infinity))
+        {
+            VideoPlayer hipnoVid = hit.collider.GetComponent<VideoPlayer>();
+            if (hipnoVid != null)
+            {
+                //esta mirando el video
+                return true
+            }
+            else
+            {
+                //no esta mirando el video
+                return false
+            }
+        }
+        //no esta mirando el video
+        return false;
+    }
+
     private void CompletarHipnosis()
     {
         Debug.Log("El jugador ha completado la hipnosis con éxito");
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject == pantallaDeHipnosis.gameObject)
-        {
-            estaHipnotizado = true;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject == pantallaDeHipnosis.gameObject)
-        {
-            estaHipnotizado = false;
-        }
+        // Agrega aquí la lógica para completar la hipnosis según tus requerimientos
     }
 }
-    
